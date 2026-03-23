10a3b3998034687002a8b20417e44ce295260e7f
phongsakorntangpok <phongsakorntangpok.dev>
2026-03-23 11:11:31 +0700

    test jenkins

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ const port: number = 3000
 // GET /
 app.get('/', (_: Request, res: Response) => {
   res.json({
-    message: 'Hello Express + TypeScript!'
+    message: 'Hello Rust + TypeScript!'
   })
 })
 
